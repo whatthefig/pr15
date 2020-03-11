e732089b84e350d1a93023a7620417aaaca20aae
Царицын Андрей Андреевич <aa_tsaritsyn@bbgroup.local>
2020-03-11 05:32:10 +0300

    commit

--- controllers/users.js
+++ controllers/users.js
@@ -44,6 +44,7 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  console.log(JWT_SECRET);
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev', { expiresIn: '7d' });
